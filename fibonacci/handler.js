7f4c8e573ee59c1119887411bbd412bf2884eb2a
dshiell <dshiell@verizon.com>
2019-06-06 21:23:08 +0000

    update to node10-express

--- fibonacci/handler.js
+++ fibonacci/handler.js
@@ -2,14 +2,13 @@
 
 module.exports = handler;
 
-async function* handler(req, res) {
+function handler(req, res) {
 
     try {
 	let p = 0
 	let s = 1
 
 	for (let i=0; i<req.query.n; i+=1) {
-	    if (i % 1000 == 0) yield null
 	    let a = s
 	    s += p
 	    p = a
@@ -17,8 +16,8 @@ async function* handler(req, res) {
 
 	//console.log("The n'th fibonacci number is: ", p)
 
-	res.status(200).send(p.toString())
+	res.status(200).succeed(p.toString())
     } catch(e) {
-	res.status(500).send(e.message)
+	res.status(500).succeed(e.message)
     }
 }
